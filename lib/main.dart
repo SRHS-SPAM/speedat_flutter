a5e5eec0c77cb2f076f1c1d4b898a3c005d1edbd
UNCROOS <any492266@gmail.com>
2024-06-29 20:44:56 +0900

    init

--- lib/main.dart
+++ lib/main.dart
@@ -9,38 +9,68 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Center(
-            child: Container(
-              width: 28,
-              height: 28,
-              clipBehavior: Clip.antiAlias,
-              decoration: ShapeDecoration(
-                color: Colors.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(4),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Stack(
+              children: [
+                Container(
+                  color: Colors.transparent,
                 ),
-                shadows: [
-                  BoxShadow(
-                    color: Color(0x1E000000),
-                    blurRadius: 2,
-                    offset: Offset(0, 0),
-                    spreadRadius: 0,
+                Positioned(
+                  top: 16,
+                  right: 0,
+                  child: Row(
+                    children: [
+                      LogoWidget(icon: Icons.person),
+                      LogoWidget(icon: Icons.nightlight_outlined),
+                      LogoWidget(icon: Icons.menu),
+                    ],
                   ),
-                  BoxShadow(
-                    color: Color(0x28000000),
-                    blurRadius: 4,
-                    offset: Offset(0, 2),
-                    spreadRadius: 0,
-                  ),
-                ],
-              ),
-              child: Image.asset('assets/images/Vector.png'), // 이미지 파일 경로 설정
+                ),
+              ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class LogoWidget extends StatelessWidget {
+  final IconData icon; // 아이콘 데이터를 받는 변수 추가
+
+  LogoWidget({required this.icon}); // 생성자에서 아이콘 데이터를 받도록 수정
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: 28,
+        height: 28,
+        clipBehavior: Clip.antiAlias,
+        decoration: ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
+          ),
+          shadows: const [
+            BoxShadow(
+              color: Color(0x1E000000),
+              blurRadius: 2,
+              offset: Offset(0, 0),
+              spreadRadius: 0,
+            ),
+            BoxShadow(
+              color: Color(0x28000000),
+              blurRadius: 4,
+              offset: Offset(0, 2),
+              spreadRadius: 0,
+            ),
+          ],
+        ),
+        child: Icon(icon), // icon 변수를 사용하여 아이콘 표시
       ),
     );
   }
