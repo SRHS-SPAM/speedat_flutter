829b2beda05ce809508a79a73ae5c4c35451acad
UNCROOS <any492266@gmail.com>
2024-07-05 23:45:56 +0900

    init

--- lib/setting_screen.dart
+++ lib/setting_screen.dart
@@ -128,7 +128,7 @@ class _SetScreenState extends State<SetScreen> {
               ),
               ListTile(
                 leading: const Icon(Icons.restaurant_menu),
-                title: const Text('급십실'),
+                title: const Text('급십표'),
                 onTap: () {
                   Navigator.pop(context); // 드로어 닫기
                   Navigator.pushNamed(context, '/bob'); // '/main'으로 이동
